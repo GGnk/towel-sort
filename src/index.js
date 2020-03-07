5f7aa3e2301d135782bda217da272907252bbf1f
GGnk <47478900+GGnk@users.noreply.github.com>
2020-03-07 21:45:22 +0400

    fix: update task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result=[];
+  if(!matrix) return result;
+  let iter=0;
+  matrix.forEach((elem) => {
+      iter+=1;
+      if (iter%2 !== 0) result = [].concat(...result ,...elem); 
+      else result = [].concat(...result, ...elem.reverse());
+  });
+  return result;
 }
